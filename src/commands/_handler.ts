9834f859a24a05dd7d03842dfdabf6c2a1b0ef02
xig1517 <xig1517@gmail.com>
2024-11-18 17:19:18 +0800

    Refactor: 移除建築相關的指令
    - 配合Commit 065d2e3

--- src/commands/_handler.ts
+++ src/commands/_handler.ts
@@ -1,27 +1,21 @@
 import { Player } from "@minecraft/server";
-import { create } from "./create";
-import { start } from "./start";
-import { exit } from "./exit";
+import { structure } from "./structure";
 import { test } from "./test";
 
 export let eventBuffer: Record<string, number> = {};
 
 function command_handler(sender: Player, cmd: string) {
-    try {
-        if (!cmd.startsWith("-build")) return false;
+    if (!cmd.startsWith("-tl") || cmd.startsWith("-townlife")) return false;
 
-        const args = cmd.split(' '); args.splice(0, 1);
-        switch (args[0]) {
-            case "create": return create(sender, args);
-            case "start": return start(sender);
-            case "exit": return exit(sender);
-            case "test": return test(sender, args);
-            default: throw "請輸入正確的指令";
-        }
-
-    } catch (errMsg: any) {
-        console.warn(errMsg);
-        return false;
+    const args = cmd.split(' '); args.splice(0, 1);
+    const cmdName = args[0]; args.splice(0, 1);
+    switch (cmdName) {
+        case "structure":
+        case "st":
+            return structure(sender, args);
+        case "test":
+            return test(sender, args);
+        default: throw Error("請輸入正確的指令");
     }
 }
-export { command_handler };
+export { command_handler }
